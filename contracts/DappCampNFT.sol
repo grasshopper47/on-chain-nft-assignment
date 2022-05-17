4a33b079f490b76660d010a56dfae9f66fdb9d97
Peter <petrut.suciu@gmail.com>
2022-05-18 00:02:09 +0200

    Updated formatting

--- contracts/DappCampNFT.sol
+++ contracts/DappCampNFT.sol
@@ -6,30 +6,54 @@ import "@openzeppelin/contracts/utils/Strings.sol";
 import "@openzeppelin/contracts/utils/Base64.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-
-contract DappCampNFT is ERC721Enumerable, Ownable {
-    uint256 public MAX_MINTABLE_TOKENS = 5;
-
-    constructor() ERC721("DappCamp NFT", "DCAMP") Ownable() {}
-
-    string[] private collection = [
+contract DappCampNFT is ERC721Enumerable, Ownable
+{
+    // -- STACK -----------------------------
+    string[] private collection =
+    [
         ""
     ];
 
-    function random(string memory input) internal pure returns (uint256) {
-        
+    uint256 public MAX_MINTABLE_TOKENS = 5;
+
+    // -- CONSTRUCTORS ----------------------
+    constructor() ERC721("DappCamp NFT", "DCAMP") Ownable() { }
+
+    // -- METHODS ---------------------------
+    function random(string memory input)
+    internal
+    pure
+    returns (uint256)
+    {
+
     }
 
-    function pluck(uint256 tokenId, string memory keyPrefix, string[] memory sourceArray) internal pure returns (string memory) {
+    function pluck
+    (
+        uint256 tokenId
+    ,   string memory keyPrefix
+    ,   string[] memory sourceArray
+    )
+    internal
+    pure
+    returns (string memory)
+    {
 
     }
 
-    function tokenURI(uint256 tokenId) override public view returns (string memory) {
+    function tokenURI(uint256 tokenId) override
+    public
+    view
+    returns (string memory)
+    {
 
     }
 
-    function claim(uint256 tokenId) public {
+    function claim(uint256 tokenId)
+    public
+    {
         require(tokenId > 0 && tokenId < MAX_MINTABLE_TOKENS, "Token ID invalid");
+
         _safeMint(_msgSender(), tokenId);
     }
 }
